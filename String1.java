c2c5a41b55ad56af8c8b83187866af5316fa6592
rsnb0y <113795159+rohityadav7964@users.noreply.github.com>
2025-07-22 17:42:23 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,12 @@
+public class String1 {
+    public static void main(String[] args) {
+
+        String s1 = "Rohit";
+        String s2 = "Yadav";
+        if (s1 == s2) {
+            System.out.println("Equal");
+        } else {
+            System.out.println("Unequal");
+        }
+    }
+}
